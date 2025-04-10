4838792224be443cd5b2cf13366614dd0dc25319
daniel_kaiser <katharsis23>
2025-04-10 23:33:41 +0300

    added routes and startup event, server succesfully starts

--- backend/views/article.py
+++ backend/views/article.py
@@ -1,7 +1,7 @@
 
 from fastapi import Depends, status, APIRouter
-from ..models import *
-from ..database import get_db, AsyncSession, ENGINE
+from models import *
+from database import get_db, AsyncSession, ENGINE
 from fastapi.responses import JSONResponse
 import json
 from pydantic import BaseModel
