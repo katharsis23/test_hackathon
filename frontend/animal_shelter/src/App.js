b080d36a95756e419a4895eb7b46cd7549eb01cb
feveron <veronika.fedyk06@gmail.com>
2025-04-13 04:41:18 +0300

    Created VolunteerCabinet page

--- frontend/animal_shelter/src/App.js
+++ frontend/animal_shelter/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginSignUp from "./pages/LoginSignUp/LoginSignUp";
 import OrganizationCabinet from "./pages/OrganizationCabinet/OrganizationCabinet";
 import ArticleForm from "./pages/ArticleForm/ArticleForm";
+import VolunteerCabinet from "./pages/VolunteerCabinet/VolunteerCabinet";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LoginSignUp />} />
         <Route path="/OrganizationCabinet" element={<OrganizationCabinet />} />
         <Route path="/ArticleForm" element={<ArticleForm />} />
+        <Route path="/VolunteerCabinet" element={<VolunteerCabinet />} />
       </Routes>
     </Router>
   );
